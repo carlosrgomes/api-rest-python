0ce2b90ed2d7ee07410943a68c9db8306f453343
Carlos Barbero <carlosrgomes@gmail.com>
2018-10-25 10:55:21 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import random
+from flask import Flask, jsonify, request
+from flasgger import Swagger, swag_from
+
+
+app = Flask(__name__)
+Swagger(app)
+
+@app.route('/api/<string:language>/', methods=['GET'])
+@swag_from('language.yml')
+def index(language):
+    language = language.lower().strip()
+    features = [
+        "awesome", "great", "dynamic", 
+        "simple", "powerful", "amazing", 
+        "perfect", "beauty", "lovely"
+    ]
+    size = int(request.args.get('size', 1))
+    if language in ['php', 'vb', 'visualbasic', 'actionscript']:
+        return "An error occurred, invalid language for awesomeness", 500
+    return jsonify(
+        language=language,
+        features=random.sample(features, size)
+    )
+
+
+app.run(debug=True)
